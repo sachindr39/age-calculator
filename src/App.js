aac690e5ea747754bbcbc7de048378f99c74d726
Sachin Raut <105543403+sachindr39@users.noreply.github.com>
2022-09-12 19:00:21 +0530

    chengess calculate work and css add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import moment from 'moment';
 import './App.css';
+import ViewsDatePicker from "./datepiker";
+
 
 function App() {
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='age calcutator'>
+
+      <div className='birth date'>
+        <ViewsDatePicker />
+
+      </div>
     </div>
+  
   );
 }
 
